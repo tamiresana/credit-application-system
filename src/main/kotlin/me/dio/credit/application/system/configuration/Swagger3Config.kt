a52952b09d559c7b47bfdf609b69c506e507e027
Tamires Ana <tamiresanap@outlook.com>
2023-05-28 17:09:58 -0300

    first commit

---
+++ src/main/kotlin/me/dio/credit/application/system/configuration/Swagger3Config.kt
@@ -0,0 +1,16 @@
+package me.dio.credit.application.system.configuration
+
+import org.springdoc.core.models.GroupedOpenApi
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class Swagger3Config {
+    @Bean
+    fun publicApi(): GroupedOpenApi? {
+        return GroupedOpenApi.builder()
+            .group("springcreditapllicationsystem-public")
+            .pathsToMatch("/api/customers/**", "/api/customers/**")
+            .build()
+    }
+}
