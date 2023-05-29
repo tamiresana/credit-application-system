9ba57561ee9097e0562e75e87cc054a54ca5b49c
Tamires Ana <tamiresanap@outlook.com>
2023-05-28 22:59:37 -0300

    testes CustomerService e CreditRepository

--- src/main/kotlin/me/dio/credit/application/system/controller/CustomerResource.kt
+++ src/main/kotlin/me/dio/credit/application/system/controller/CustomerResource.kt
@@ -17,12 +17,13 @@ class CustomerResource(
 ) {
 
   @PostMapping
-  fun saveCustomer(@RequestBody @Valid customerDto: CustomerDto): ResponseEntity<String> {
-    val savedCustomer = this.customerService.save(customerDto.toEntity())
-    return ResponseEntity.status(HttpStatus.CREATED).body("Customer ${savedCustomer.email} saved!")
+  fun saveCustomer(@RequestBody @Valid customerDto: CustomerDto): ResponseEntity<CustomerView> {
+    val savedCustomer: Customer = this.customerService.save(customerDto.toEntity())
+    return ResponseEntity.status(HttpStatus.CREATED).body(CustomerView(savedCustomer))
   }
 
   @GetMapping("/{id}")
+
   fun findById(@PathVariable id: Long): ResponseEntity<CustomerView> {
     val customer: Customer = this.customerService.findById(id)
     return ResponseEntity.status(HttpStatus.OK).body(CustomerView(customer))
